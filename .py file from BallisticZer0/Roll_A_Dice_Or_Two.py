c2618ec7ceaa34f4eeb4be4c5d9cb066afce003b
unknown <you@example.com>
2018-10-04 20:42:31 -0600

    Added so its secure in 2.7

--- .py file from BallisticZer0/Roll_A_Dice_Or_Two.py
+++ .py file from BallisticZer0/Roll_A_Dice_Or_Two.py
@@ -6,9 +6,10 @@ import random
 
 
 def One_OR_Two():  #Right here its saying that for number 1 press it will roll one dice And when number 2 pressed it will roll 2 and ect.
-    if input("Do you want 1 dice or 2?  \n\1 for 1(Die) \n2 for 2(Dice)  :") == '1':
+    if raw_input("Do you want 1 dice or 2?  \n\1 for 1(Die) \n2 for 2(Dice)  :") == '1':
         print("The dice rolled",random.randint(1,6))
         time.sleep(1)
+    break
 
     else:           # here its saying that when it prints dice rolled it also prints a random randint from 1 to 6, this is also happining for the second Dice.
         print("\n\nThe dice rolled\n\n",random.randint(1,6),"\n\n","And\n\n",random.randint(1,6),"\n")
@@ -19,12 +20,10 @@ def One_OR_Two():  #Right here its saying that for number 1 press it will roll o
 
 
         # This is saying if the input to do you want to roll a dice says y or yes then it puts a random randint from a dice.
-while input("\n\nDo you want to roll a dice? [y|n]") == 'y':
+while raw_input("\n\nDo you want to roll a dice? [y|n]") == 'y':
     One_OR_Two()
 
-
-
-
+    break
 else:
      print("Maybe next time")
 
